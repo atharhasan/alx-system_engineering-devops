61718b4f29fab8e097be5635ed910bb4cf277214
atharhasan <athar_h12@yahoo.com>
2024-03-22 11:25:51 +0200

    commit task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,16 @@
-# install puppet-lint
+# 1-install_a_package.pp
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+package { 'python3-pip':
+  ensure => installed,
+}
+
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install Flask==2.1.0',
+  path        => ['/usr/bin', '/bin'],
+  refreshonly => true,
+}
+
+package { 'Flask':
+  ensure  => '2.1.0',
+  require => Exec['install_flask'],
 }
