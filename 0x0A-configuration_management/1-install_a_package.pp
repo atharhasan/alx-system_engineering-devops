344da7c65ddde3ffcf6d0446fe2386c66f5c981f
atharhasan <athar_h12@yahoo.com>
2024-03-22 11:27:36 +0200

    commit task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,17 +1,6 @@
 #!/usr/bin/pup
-# 1-install_a_package.pp
-
-package { 'python3-pip':
-  ensure => installed,
-}
-
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install Flask==2.1.0',
-  path        => ['/usr/bin', '/bin'],
-  refreshonly => true,
-}
-
-package { 'Flask':
-  ensure  => '2.1.0',
-  require => Exec['install_flask'],
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
