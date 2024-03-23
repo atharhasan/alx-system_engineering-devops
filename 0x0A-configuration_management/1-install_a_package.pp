8d1f16e594bfd2d61f10f366df81318459263943
atharhasan <athar_h12@yahoo.com>
2024-03-23 09:11:25 +0200

    commit task

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,10 +1,11 @@
 # Install puppet-lint
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
 }
 
 package { 'werkzeug':
-ensure => '2.1.1',
-provider => 'pip3',
+	ensure => '2.1.1',
+	provider => 'pip3',
 }
