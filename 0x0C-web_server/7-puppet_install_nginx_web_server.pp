2e502dee36bf6adaccddf0b7382dd92fab87ae0a
atharhasan <athar_h12@yahoo.com>
2024-03-27 16:49:57 +0200

    commit task

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,30 +1,20 @@
-#!/usr/bin/env bash
-# Configure your Nginx server to have a custom 404 page
+# Install Nginx
+package { 'nginx':
+  ensure => installed,
+}
 
-apt-get update
-apt-get install -y nginx
+file_line { 'aaaaa':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+}
 
-mkdir /etc/nginx/html
-touch /etc/nginx/html/index.html
-echo "Hello World!" > /etc/nginx/html/index.html
-touch /etc/nginx/html/404.html
-echo "Ceci n'est pas une page" > /etc/nginx/html/404.html
+file { '/var/www/html/index.html':
+  content => 'Hello World!',
+}
 
-printf %s "server {
-    listen 80;
-    listen [::]:80 default_server;
-    root   /etc/nginx/html;
-    index  index.html index.htm;
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    error_page 404 /404.html;
-    location /404 {
-      root /etc/nginx/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
